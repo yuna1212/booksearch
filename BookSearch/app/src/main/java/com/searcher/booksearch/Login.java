047129824121214a4fe3ec6185ba5c1139cc9007
Park-Ji-Eun <penny0123@naver.com>
2020-10-31 16:23:25 +0900

    로그인 페이지 구현

---
+++ BookSearch/app/src/main/java/com/searcher/booksearch/Login.java
@@ -0,0 +1,35 @@
+package com.searcher.booksearch;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.searcher.booksearch.R;
+
+public class Login extends AppCompatActivity {
+
+    public static final int REQUEST_CODE_SELETMENU = 101;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        //loginButton 클릭했을 때, selectmenu로 전환
+        Button loginButton = (Button) findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SelectMenu.class);
+                startActivityForResult(intent, REQUEST_CODE_SELETMENU);
+            }
+        });
+    }
+}
