bef5e6faad80418786394f012f06a9ed8fa8491a
Park-Ji-Eun <penny0123@naver.com>
2020-10-31 16:23:36 +0900

    바코드 스캔 구현

---
+++ BookSearch/app/src/main/java/com/searcher/booksearch/ScanBarcode.java
@@ -0,0 +1,41 @@
+package com.searcher.booksearch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import org.searcher.booksearch.R;
+
+public class ScanBarcode extends AppCompatActivity {
+    private IntentIntegrator barcodeScan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.scan_barcode_activity);
+
+        barcodeScan = new IntentIntegrator(this);
+        barcodeScan.setOrientationLocked(false); // default가 세로모드인데 휴대폰 방향에 따라 가로, 세로로 자동 변경됩니다.
+        barcodeScan.initiateScan();
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+                // todo
+            } else {
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+                // todo
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+}
