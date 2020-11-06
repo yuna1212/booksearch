e5cf8742727d153d348ac81b094e9069cab133da
Park-Ji-Eun <penny0123@naver.com>
2020-11-07 04:07:36 +0900

    데이터베이스에 있는 데이터 리싸이클러뷰 사용하여 보여주기

--- android/BookSearch/app/src/main/java/com/searcher/booksearch/MyBookList.java
+++ android/BookSearch/app/src/main/java/com/searcher/booksearch/MyBookList.java
@@ -1,5 +1,6 @@
 package com.searcher.booksearch;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,11 +24,23 @@ public class MyBookList extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
         MyBookListItemAdapter adapter = new MyBookListItemAdapter();
 
+        // 데이터베이스에 존재하는 데이터들 카드뷰로 출력
+        ManageDatabase manageDatabase = new ManageDatabase(this);
+        Cursor cursor = manageDatabase.selectData();
+
+        int recordCount = cursor.getCount();    // 데이터베이스 내에 있는 데이터 개수 저장
+
+        // 현재 ISBN 정보 보이도록 구현해둔 상태
+        for (int i = 0; i < recordCount; i++) {
+            cursor.moveToNext();
+            adapter.addItem(new MyBookListItem(cursor.getString(0)));
+        }
+
+        cursor.close();
+
         // 테스트
         adapter.addItem(new MyBookListItem("테스트1"));
         adapter.addItem(new MyBookListItem("테스트2"));
-        adapter.addItem(new MyBookListItem("테스트3"));
-        adapter.addItem(new MyBookListItem("테스트4"));
 
         recyclerView.setAdapter(adapter);
     }
